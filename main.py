f1a1fe742cbbf95dcfeab3d01a885db33f2c2c54
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2022-07-18 09:53:54 -0500

    Phones are ready to use now

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
     usersMails = []
+    usersPhones = []
 
     def countEmails(ID):
-        emailCount = 0
+        emailcount = 0
         for user in usersMails:
             if user.ID == ID:
-                emailCount += 1
-        return emailCount
+                emailcount += 1
+        return emailcount
 
+    def countPhones(ID):
+        phonecount = 0
+        for user in usersPhones:
+            if user.ID == ID:
+                phonecount += 1
+        return phonecount
 
-    def checkIDPosition(ID):
+    def checkIDPositionInMails(ID):
         for i in range(len(usersMails)):
             if usersMails[i].ID == ID:
                 return i
         return -1
 
+    def checkIDPositionInPhones(ID):
+        for i in range(len(usersPhones)):
+            if usersPhones[i].ID == ID:
+                return i
+        return -1
 
     def clearMoreThanThreeEmailsByUser():
         for user in usersMails:
             while countEmails(user.ID) > 3:
-                usersMails.pop(checkIDPosition(user.ID))
+                usersMails.pop(checkIDPositionInMails(user.ID))
 
+    def clearMoreThanThreePhonesByUser():
+        for user in usersPhones:
+            while countPhones(user.ID) > 3:
+                usersPhones.pop(checkIDPositionInPhones(user.ID))
 
     uno = pd.read_excel('C:\\Users\\kevin\\OneDrive\\Escritorio\\uno.xls', sheet_name='Contactos')
 
-    IDList = uno['ID'].unique().tolist()
-    EmailList = uno['Email'].unique().tolist()
+    idList = uno['ID'].unique().tolist()
+    emailList = uno['Email'].unique().tolist()
+    phoneList = uno['Teléfono'].unique().tolist()
 
     class User():
         def __init__(self, ID, Field, FieldCount):
@@ -39,29 +54,47 @@ if __name__ == '__main__':
             self.FieldCount = FieldCount
 
 
-    for id in IDList:
-        for email in EmailList:
+    # add emails to the email list
+    for id in idList:
+        for email in emailList:
             if email in uno[uno['ID'] == id]['Email'].values:
-                # add in the first position of the list
                 usersMails.insert(0, User(id, email, uno[uno['ID'] == id]['Email'].value_counts()[email]))
 
+    # add phones to the phones list
+    for id in idList:
+        for phone in phoneList:
+            if phone in uno[uno['ID'] == id]['Teléfono'].values:
+                usersPhones.insert(0, User(id, phone, uno[uno['ID'] == id]['Teléfono'].value_counts()[phone]))
+
     usersMails.sort(key=lambda x: x.FieldCount, reverse=False)
+    usersPhones.sort(key=lambda x: x.FieldCount, reverse=False)
+
     clearMoreThanThreeEmailsByUser()
+    clearMoreThanThreePhonesByUser()
 
     uno.drop(columns=['Email'], inplace=True)
+    uno.drop(columns=['Teléfono'], inplace=True)
 
     dos = pd.DataFrame(uno)
     dos.drop_duplicates(subset=['ID'], inplace=True)
 
     usersMails.sort(key=lambda x: x.FieldCount, reverse=True)
+    usersPhones.sort(key=lambda x: x.FieldCount, reverse=True)
 
-    for id in IDList:
+    for id in idList:
         aux = 1;
-        while checkIDPosition(id) != -1:
-            dos.loc[dos['ID'] == id, 'Email_'+str(aux)] = usersMails[checkIDPosition(id)].Field
-            usersMails.pop(checkIDPosition(id))
+        while checkIDPositionInMails(id) != -1:
+            dos.loc[dos['ID'] == id, 'Email_'+str(aux)] = usersMails[checkIDPositionInMails(id)].Field
+            usersMails.pop(checkIDPositionInMails(id))
             aux += 1
 
-    dos.to_csv('new.csv', index=False, encoding="latin1")
+    for id in idList:
+        aux = 1;
+        while checkIDPositionInPhones(id) != -1:
+            dos.loc[dos['ID'] == id, 'Teléfono_'+str(aux)] = usersPhones[checkIDPositionInPhones(id)].Field
+            usersPhones.pop(checkIDPositionInPhones(id))
+            aux += 1
 
-# create a list with the ID and the email and name
+    dos.to_csv('C:\\Users\\kevin\\PycharmProjects\\EmailAnalyse\\newFile.csv', index=False, encoding="latin1")
+
+    print('Listo')
--- test.py
+++ test.py
@@ -5,39 +5,20 @@ import numpy as np
 uno = pd.read_excel('C:\\Users\\kevin\\OneDrive\\Escritorio\\uno.xls', sheet_name='Contactos')
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        #check if the ID have one email
+    def checkID12HaveEmail(self):
         val = 'africa@altecom.es' in uno[uno['ID'] == 12]['Email'].values
-        # value = uno[uno['ID'] == 2]['Email'].value_counts()['agata@hotmail.com'] > 0
         self.assertEqual(True, val)  # add assertion here
 
     def test_something_else(self):
         uno = pd.read_excel('C:\\Users\\kevin\\OneDrive\\Escritorio\\uno.xls', sheet_name='Contactos')
 
-        ids = uno['ID'].unique()
-
-        # get just one row for each ID
-        dos = open('new.csv', 'w', encoding="utf-8")
-
         uno.drop_duplicates(subset=['Email','ID'], inplace=True)
         uno.drop(columns=['Email'], inplace=True)
 
-
         dos = pd.DataFrame(uno)
         print(dos)
-        #
-        # for id in ids:
-        #     dos.write(uno.columns)
-        #     dos.write(uno[uno['ID'] == id].head(1).values.tostring())
-        #     dos.write('\n')
-        # dos.close()
-        # # self.assertEqual(True, value)  # add assertion here
-        #
-        # print(dos.head())
-        # print(ids)
-        #
-        #
-        # print(uno[uno['ID'] == 20].head(1))
+
+        dos.to_csv('C:\\Users\\kevin\\PycharmProjects\\EmailAnalyse\\newFile.csv', index=False, encoding="latin1")
 
 if __name__ == '__main__':
     unittest.main()
