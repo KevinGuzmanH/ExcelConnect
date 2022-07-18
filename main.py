a278a6f0ba1501c88b22f4f3cf91a867c0024dc8
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2022-07-18 15:19:18 -0500

    Comments added

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 if __name__ == '__main__':
 
+    class User():
+        def __init__(self, ID, Field, FieldCount):
+            self.ID = ID
+            self.Field = Field
+            self.FieldCount = FieldCount
+
     usersMails = []
     usersPhones = []
 
@@ -41,60 +47,62 @@ if __name__ == '__main__':
             while countPhones(user.ID) > 3:
                 usersPhones.pop(checkIDPositionInPhones(user.ID))
 
-    uno = pd.read_excel('C:\\Users\\kevin\\OneDrive\\Escritorio\\uno.xls', sheet_name='Contactos')
+    # Read the excel file to use
+    oldEx = pd.read_excel('C:\\Users\\kevin\\OneDrive\\Escritorio\\uno.xls', sheet_name='Contactos')
 
-    idList = uno['ID'].unique().tolist()
-    emailList = uno['Email'].unique().tolist()
-    phoneList = uno['Teléfono'].unique().tolist()
-
-    class User():
-        def __init__(self, ID, Field, FieldCount):
-            self.ID = ID
-            self.Field = Field
-            self.FieldCount = FieldCount
-
+    # get the ids, emails and phones without repetitions
+    idList = oldEx['ID'].unique().tolist()
+    emailList = oldEx['Email'].unique().tolist()
+    phoneList = oldEx['Teléfono'].unique().tolist()
 
     # add emails to the email list
     for id in idList:
         for email in emailList:
-            if email in uno[uno['ID'] == id]['Email'].values:
-                usersMails.insert(0, User(id, email, uno[uno['ID'] == id]['Email'].value_counts()[email]))
+            if email in oldEx[oldEx['ID'] == id]['Email'].values:
+                usersMails.insert(0, User(id, email, oldEx[oldEx['ID'] == id]['Email'].value_counts()[email]))
 
     # add phones to the phones list
     for id in idList:
         for phone in phoneList:
-            if phone in uno[uno['ID'] == id]['Teléfono'].values:
-                usersPhones.insert(0, User(id, phone, uno[uno['ID'] == id]['Teléfono'].value_counts()[phone]))
+            if phone in oldEx[oldEx['ID'] == id]['Teléfono'].values:
+                usersPhones.insert(0, User(id, phone, oldEx[oldEx['ID'] == id]['Teléfono'].value_counts()[phone]))
 
+    # organize the emails and phones lists by least repetitions to delete the least repeated
     usersMails.sort(key=lambda x: x.FieldCount, reverse=False)
     usersPhones.sort(key=lambda x: x.FieldCount, reverse=False)
 
+    # remove emails and phones with more than three repetitions
     clearMoreThanThreeEmailsByUser()
     clearMoreThanThreePhonesByUser()
 
-    uno.drop(columns=['Email'], inplace=True)
-    uno.drop(columns=['Teléfono'], inplace=True)
+    # remove the emails and phones columns from the oldEx dataframe
+    oldEx.drop(columns=['Email'], inplace=True)
+    oldEx.drop(columns=['Teléfono'], inplace=True)
 
-    dos = pd.DataFrame(uno)
-    dos.drop_duplicates(subset=['ID'], inplace=True)
+    # create a new dataframe with the same columns and rows as the new dataframe without ID repetitions
+    newEx = pd.DataFrame(oldEx)
+    newEx.drop_duplicates(subset=['ID'], inplace=True)
 
+    # organize the emails and phones lists by highest repetitions to take first the most repeated
     usersMails.sort(key=lambda x: x.FieldCount, reverse=True)
     usersPhones.sort(key=lambda x: x.FieldCount, reverse=True)
 
+    # add the emails and phones columns to the new dataframe
     for id in idList:
         aux = 1;
         while checkIDPositionInMails(id) != -1:
-            dos.loc[dos['ID'] == id, 'Email_'+str(aux)] = usersMails[checkIDPositionInMails(id)].Field
+            newEx.loc[newEx['ID'] == id, 'Email_' + str(aux)] = usersMails[checkIDPositionInMails(id)].Field
             usersMails.pop(checkIDPositionInMails(id))
             aux += 1
 
     for id in idList:
         aux = 1;
         while checkIDPositionInPhones(id) != -1:
-            dos.loc[dos['ID'] == id, 'Teléfono_'+str(aux)] = usersPhones[checkIDPositionInPhones(id)].Field
+            newEx.loc[newEx['ID'] == id, 'Teléfono_' + str(aux)] = usersPhones[checkIDPositionInPhones(id)].Field
             usersPhones.pop(checkIDPositionInPhones(id))
             aux += 1
 
-    dos.to_csv('C:\\Users\\kevin\\PycharmProjects\\EmailAnalyse\\newFile.csv', index=False, encoding="latin1")
+    # save the new dataframe to a csv file
+    newEx.to_csv('C:\\Users\\kevin\\PycharmProjects\\EmailAnalyse\\newFile.csv', index=False, encoding="latin1")
 
     print('Listo')
